072af15c5e4f11f12461e296d7e1c61fe39c927b
java_DSA <jayprakashmcode7@gmail.com>
2023-12-17 01:01:28 +0530

    Addinf the file ocde that returns the result of the product_of_array_except_self question.

---
+++ LeetCode_100Days/DAY16_Product_Of_Array_Except_self.java
@@ -0,0 +1,45 @@
+package Leetcode.LeetCode_100Days;
+
+class Solution16 {
+    public int[] productExceptSelf(int[] nums) {
+
+        // right
+        int right[] = new int[nums.length];
+        int p = 1;
+        int n = nums.length;
+        for (int i = n - 1; i >= 0; i--) {
+            p = p * nums[i];
+            right[i] = p;
+        }
+
+        int ans[] = new int[n];
+        int left = 1;
+
+        for (int i = 0; i < n - 1; i++) {
+            int val = left * right[i + 1];
+            ans[i] = val;
+            left = left * nums[i];
+        }
+        ans[n - 1] = left;
+
+        return ans;
+    }
+}
+
+public class DAY16_Product_Of_Array_Except_self {
+    public static void printArray(int array[]) {
+        for (int i = 0; i < array.length; i++) {
+            System.out.print(array[i] + " ");
+        }
+    }
+
+    public static void main(String[] args) {
+
+        Solution16 s = new Solution16();
+        int[] nums = { 1, 2, 3, 4 };
+
+        int result[] = s.productExceptSelf(nums);
+        printArray(result);
+
+    }
+}
